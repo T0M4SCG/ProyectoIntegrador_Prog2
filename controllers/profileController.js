211a6935780a1c97979946237521594dc94b5b27
T0M4SCG <tconchagiovana@udesa.edu.ar>
2023-04-11 19:57:50 -0300

    Todo lo que faltaba creo

--- controllers/profileController.js
+++ controllers/profileController.js
@@ -3,7 +3,7 @@ let data = require("../data/data")
 
 let profiles = {
     index: function (req,res) {
-        return res.render("profile",{data: data.usuarios})
+        return res.render("profile",{data: data.usuarios, productos:data.productos})
     },
     edit:function (req,res) {
         
